34b617518383b609dcaf46d9ae824cd2c3709e90
ChrisKnowsDev <chrisharley81@gmail.com>
2024-04-10 10:47:40 -0400

    landing page UI complete

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,5 +1,25 @@
+import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 
 export default function Home() {
-  return <main className="text-6xl">start here</main>
+  return (
+    <main className="px-2 flex flex-col justify-center min-h-screen max-w-3xl mx-auto lg:flex-row lg:items-center lg:gap-8 lg:max-w-6xl">
+      <Image src="../icon.svg" alt="hero" width={762} height={762} />
+
+      <div className="mt-16">
+        <h1 className="text-4xl font-bold sm:text-5xl">
+          Manage your carwash with ease
+        </h1>
+        <p className="text-slate-700 pt-2">
+          Easily keep track of your clients and cars with{' '}
+          <span className="text-blue-700 font-bold text-lg">washlist</span>. Get
+          lifetime access for $3.99
+        </p>
+        <div className="pt-16 flex gap-2">
+          <Button>Get started</Button>
+          <Button variant="outline">Get started</Button>
+        </div>
+      </div>
+    </main>
+  )
 }
