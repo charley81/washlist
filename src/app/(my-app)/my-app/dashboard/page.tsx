87242dcf8cd163bec1af508aa1ec17002aab4fd9
ChrisKnowsDev <chrisharley81@gmail.com>
2024-04-11 16:28:51 -0400

    created inital client search component file

--- src/app/(my-app)/my-app/dashboard/page.tsx
+++ src/app/(my-app)/my-app/dashboard/page.tsx
@@ -1,9 +1,11 @@
 import ClientCount from '@/components/client-count'
 import ClientScrollMenu from '@/components/client-scroll-menu'
+import ClientSearch from '@/components/client-search'
 
 export default function DashboardPage() {
   return (
     <main>
+      <ClientSearch />
       <ClientCount />
       <ClientScrollMenu />
     </main>
