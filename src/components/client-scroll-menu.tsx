4a32b4dec615cf7eeb24f92393b342962d2023f6
ChrisKnowsDev <chrisharley81@gmail.com>
2024-04-15 16:10:59 -0400

    cheveron function complete on client list scroll area

--- src/components/client-scroll-menu.tsx
+++ src/components/client-scroll-menu.tsx
@@ -1,11 +1,26 @@
+'use client'
+
+import { useState } from 'react'
 import { ScrollArea } from '@/components/ui/scroll-area'
 import { Separator } from '@/components/ui/separator'
 import { tempClients } from '@/lib/temp-data'
+import { ChevronDown } from 'lucide-react'
 
 export default function ClientScrollMenu() {
+  const [isAtBottom, setIsAtBottom] = useState(false)
+
+  const checkScrollPosition = (event) => {
+    const { scrollTop, scrollHeight, clientHeight } = event.target
+    const atBottom = scrollTop + clientHeight >= scrollHeight
+    setIsAtBottom(atBottom)
+  }
+
   return (
     <div>
-      <ScrollArea className="h-48 rounded-md mt-6">
+      <ScrollArea
+        className="h-48 rounded-md mt-6"
+        onScroll={checkScrollPosition}
+      >
         {tempClients.map((client) => (
           <>
             <p className="text-base text-slate-700 py-4 hover:cursor-pointer">
@@ -15,6 +30,7 @@ export default function ClientScrollMenu() {
           </>
         ))}
       </ScrollArea>
+      <ChevronDown className={`mx-auto ${isAtBottom ? 'rotate-180' : ''}`} />
     </div>
   )
 }
